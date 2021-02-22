e87caa7129937032f286ebf86c83885d3b9b1bba
user1 <pooya_srfm@.yahoo.com>
2021-02-22 22:51:45 +0330

    the whole weblog

--- weblog/theblog/views.py
+++ weblog/theblog/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.urls import reverse_lazy
 
-# Create your views here.
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    ordering = ['-post_date']
+    #ordering = ['-id']
 
-def home(request):
-    return render(request, 'home.html', {}) 
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    fields = ['topic', 'secondtopic', 'message', 'tag', 'image']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
